3a840201f752499d3b3cc2fcac664ad9b414284f
My God <My God@HARDIK>
2020-06-16 18:45:33 -0700

    New changes

--- Jaydeep_Practical/src/main/java/com/main/common.java
+++ Jaydeep_Practical/src/main/java/com/main/common.java
@@ -2,8 +2,6 @@ package com.main;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
 
 public class common {
 
